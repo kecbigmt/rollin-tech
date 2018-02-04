ed483bd132fa54995bd037e9e845f2046178ec37
kecy <lpfmp.ae.k@gmail.com>
2018-02-05 01:39:12 +0900

    fff

--- 20180204_python_basics#1/function.py
+++ 20180204_python_basics#1/function.py
@@ -12,9 +12,3 @@ print(name)
 h = 170
 name = check(h)
 print(name)
-
-
-curl -X POST -H 'Authorization: Bearer xoxp-184717347331-189088339746-243930928130-2f2e7d9eacb6b3eaae75b3146f14f6db' \
--H 'Content-type: application/json' \
---data '{"channel":"C94L04EDV","text":"nya","attachments": []}' \
-https://slack.com/api/chat.postMessage
